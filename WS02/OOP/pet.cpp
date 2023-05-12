3e679d7c803162e90ff45a71d5d4d28de710b82b
Jacopostortini <jacostortini@gmail.com>
2023-05-12 17:50:43 +0200

    1

---
+++ WS02/OOP/pet.cpp
@@ -0,0 +1,18 @@
+#include "pet.h"
+#include <iostream>
+
+namespace pet {
+	void Pet::printInfo() {
+		std::cout << m_name << ", " << m_age << " years old." << std::endl;
+	}
+
+	void Dog::printInfo() {
+		Pet::printInfo();
+		std::cout << m_dogTypeMap.at(m_type) << std::endl;
+	}
+
+	void Cat::printInfo() {
+		Pet::printInfo();
+		std::cout << m_catTypeMap.at(m_type) << std::endl;
+	}
+}
--- WS02/OOP/pet.h
+++ WS02/OOP/pet.h
@@ -1,8 +1,21 @@
 #pragma once
 
 #include <string>
+#include <map>
 
 namespace pet {
+	enum class DogType {
+		LABRADOR,
+		PITBULL,
+		HOUND
+	};
+
+	enum class CatType {
+		A,
+		B,
+		C
+	};
+
 	class Pet {
 	public:
 		Pet(std::string name, unsigned int age) : m_name(name), m_age(age) {};
@@ -11,5 +24,33 @@ namespace pet {
 	private:
 		std::string m_name;
 		unsigned int m_age;
+	};
+
+	class Dog : public Pet {
+	public:
+		Dog(std::string name, unsigned int age, DogType type) : Pet(name, age), m_type(type) {};
+		~Dog() {};
+		void printInfo() override;
+	private:
+		DogType m_type;
+		std::map<DogType, std::string> m_dogTypeMap{
+			{DogType::LABRADOR, "Labrador"},
+			{DogType::PITBULL, "Pitbull"},
+			{DogType::HOUND, "Mastino"}
+		};
+	};
+
+	class Cat : public Pet {
+	public:
+		Cat(std::string name, unsigned int age, CatType type) : Pet(name, age), m_type(type) {};
+		~Cat() {};
+		void printInfo() override;
+	private:
+		CatType m_type;
+		std::map<CatType, std::string> m_catTypeMap{
+			{CatType::A, "Non so"},
+			{CatType::B, "nessuna razza"},
+			{CatType::C, "di gatto"}
+		};
 	};
 }
